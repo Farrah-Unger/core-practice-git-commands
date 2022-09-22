09eebb904cec2aa749d55eb171266a2a91aba728
ashanti <ammanier2@gmail.com>
2022-09-22 00:55:36 -0400

    "refactors always_returns_true() to return True"

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pytest
 
 
 def always_returns_true():
-    return False
+    return True
 
 
 def test_always_returns_true():
-    assert always_returns_true()
+    #act 
+    always_returns_true()
+    #assert
+    assert always_returns_true() == True
